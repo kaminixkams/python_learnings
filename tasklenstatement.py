ade239360434746864ec983807d087ae3dd1ff0b
kaminixkams <finnakms2024@gmail.com>
2024-04-18 19:43:06 +0530

    added len statement

---
+++ tasklenstatement.py
@@ -0,0 +1,77 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:/Users/kamsaxel/Documents/tasklencode.py ============
+Enter the sentence: monika
+2
+3
+4
+
+============ RESTART: C:/Users/kamsaxel/Documents/tasklencode.py ============
+Enter the sentence: monika
+1
+2
+3
+4
+
+============ RESTART: C:/Users/kamsaxel/Documents/tasklencode.py ============
+Enter the sentence: monika
+1
+2
+3
+
+============ RESTART: C:/Users/kamsaxel/Documents/tasklencode.py ============
+Enter the sentence: monika
+3
+
+============ RESTART: C:/Users/kamsaxel/Documents/tasklencode.py ============
+Enter the sentence: monika
+4
+
+============ RESTART: C:/Users/kamsaxel/Documents/tasklencode.py ============
+Enter the sentence: monika
+3
+
+============ RESTART: C:/Users/kamsaxel/Documents/tasklencode.py ============
+Enter the sentence: monika
+6
+>>> 
+============ RESTART: C:/Users/kamsaxel/Documents/tasklencode.py ============
+Enter the sentence: i am a dork 
+21
+>>> 
+============ RESTART: C:/Users/kamsaxel/Documents/tasklencode.py ============
+Enter the sentence: hello world
+Traceback (most recent call last):
+  File "C:/Users/kamsaxel/Documents/tasklencode.py", line 11, in <module>
+    l+=i
+TypeError: unsupported operand type(s) for +=: 'int' and 'str'
+>>> 
+============ RESTART: C:/Users/kamsaxel/Documents/tasklencode.py ============
+Enter the sentence: hello world
+Traceback (most recent call last):
+  File "C:/Users/kamsaxel/Documents/tasklencode.py", line 11, in <module>
+    l+=i
+TypeError: unsupported operand type(s) for +=: 'int' and 'str'
+>>> 
+============ RESTART: C:/Users/kamsaxel/Documents/tasklencode.py ============
+Enter the sentence: hello world
+Traceback (most recent call last):
+  File "C:/Users/kamsaxel/Documents/tasklencode.py", line 10, in <module>
+    for i in l:
+TypeError: 'int' object is not iterable
+>>> 
+============ RESTART: C:/Users/kamsaxel/Documents/tasklencode.py ============
+Enter the sentence: hello world
+Traceback (most recent call last):
+  File "C:/Users/kamsaxel/Documents/tasklencode.py", line 10, in <module>
+    for i in a:
+TypeError: 'int' object is not iterable
+>>> 
+============ RESTART: C:/Users/kamsaxel/Documents/tasklencode.py ============
+Enter the sentence: hello world 
+21
+>>> 
+============ RESTART: C:/Users/kamsaxel/Documents/tasklencode.py ============
+Enter the sentence: hello world
+66
